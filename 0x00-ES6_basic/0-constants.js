617a0fef8f65ead65d3176870b3844d44dd69e7e
Shighi <daisyshighi@gmail.com>
2025-04-16 07:35:45 +0000

    task 0

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,6 +1,4 @@
-// Modifying tasks to use const and let instead of var
 export function taskFirst() {
-  // Using const as the variable won't be reassigned
   const task = 'I prefer const when I can.';
   return task;
 }
@@ -10,7 +8,6 @@ export function getLast() {
 }
 
 export function taskNext() {
-  // Using let as the variable will be modified
   let combination = 'But sometimes let';
   combination += getLast();
 
